222ea91f85046a7fa945a89d6c336fc9123081ee
Robert Fletcher <robrotheram@gmail.com>
2016-10-25 16:35:50 +0100

    Project now has view rooter and usmin for building the project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <hello></hello>
+
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+          <div class="container">
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Bootstrap theme</a>
+              </div>
+              <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav">
+                      <li><router-link to="/">home</router-link></li>
+                      <li><router-link to="/foo">foo</router-link></li>
+                      <li><router-link to="/bar">bar</router-link></li>
+                  </ul>
+              </div><!--/.nav-collapse -->
+          </div>
+      </nav>
+
+
+
+      <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Hello from './components/Hello.vue'
+import Foo from './components/FOO.vue'
+import Bar from './components/BAR.vue'
+
+import VueRouter from 'vue-router'
+
+
+import "./style/main.css";
+
+
+
+// 1. Use plugin.
+// This installs <router-view> and <router-link>,
+// and injects $router and $route to all router-enabled child components
+Vue.use(VueRouter)
+
+// 2. Define route components
+
+// 3. Create the router
+const router = new VueRouter({
+    //mode: 'html5',
+    //base: __dirname,
+    routes: [
+        { path: '/', component: Hello },
+        { path: '/foo', component: Foo },
+        { path: '/bar', component: Bar }
+    ]
+})
+
+
+
 new Vue({ // eslint-disable-line no-new
   el: '#app',
+    router,
   render: (h) => h(App)
 })
